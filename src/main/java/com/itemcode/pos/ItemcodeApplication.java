8b514e4f7e512cd1cd4949c0247fcf76ab751b2b
Mai Thành Duy An <tiachop0102@gmail.com>
2021-10-31 13:53:37 +0900

    Create New ItemCode Project.

---
+++ src/main/java/com/itemcode/pos/ItemcodeApplication.java
@@ -0,0 +1,13 @@
+package com.itemcode.pos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemcodeApplication.class, args);
+	}
+
+}
